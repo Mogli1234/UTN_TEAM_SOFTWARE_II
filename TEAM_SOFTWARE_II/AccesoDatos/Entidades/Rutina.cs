d6d56e8b89638b53e78adcf690d2b7de3af5c492
Jaime Esquivel <jaimeesquivelgomez@gmail.com>
2015-04-12 12:35:57 -0600

    asdasdasda

--- TEAM_SOFTWARE_II/AccesoDatos/Entidades/Rutina.cs
+++ TEAM_SOFTWARE_II/AccesoDatos/Entidades/Rutina.cs
@@ -9,6 +9,6 @@ namespace AccesoDatos.Entidades
     class Rutina
     {
         public int id { get; set; }
-        public string rutina { get; set; }
+        public string rutina { get; set; } 
     }
 }
