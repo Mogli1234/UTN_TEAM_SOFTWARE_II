53382475af5738865eb3117dcef71835256524e8
josevayac <josevayac@gmail.com>
2015-04-12 12:43:08 -0600

    Entidades Usuario, Rutina creadas o actualizadas

--- TEAM_SOFTWARE_II/AccesoDatos/Entidades/Rutina.cs
+++ TEAM_SOFTWARE_II/AccesoDatos/Entidades/Rutina.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AccesoDatos.Entidades
 {
-    class Rutina
+    public class Rutina
     {
         public int id { get; set; }
         public string rutina { get; set; }
