ab85314c469b593de3b52cd9aae21605e9f459bc
Jaime Esquivel <jaimeesquivelgomez@gmail.com>
2015-04-20 16:23:05 -0600

    ya funciona cargar los combox de los deporte con rutinas todavia no esta el insert

--- TEAM_SOFTWARE_II/Project_View/Assigned Forms/frm_assignar_sport.cs
+++ TEAM_SOFTWARE_II/Project_View/Assigned Forms/frm_assignar_sport.cs
@@ -7,12 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Project_View.ServicioRutina;
 
 namespace Project_View
 {
     public partial class frm_assignar_sport : Form
     {
-        
+        private ServicioDeporte.DeportesWebServiceClient oSportClient = new ServicioDeporte.DeportesWebServiceClient();
+        private  ServicioRutina.RutinasClient oRutinasClient = new RutinasClient();
         public frm_assignar_sport()
         {
             InitializeComponent();
@@ -32,13 +34,14 @@ namespace Project_View
         void ChargeControls() 
         {
             //Cargo los deportes
-            
+            cmbSport.DataSource = oSportClient.ChargeSportsList();
+            cmbSport.ValueMember = "id";
             cmbSport.DisplayMember = "Deporte";
 
             //Cargo las rutinas en este 
-          
+            cmbRutine.DataSource = oRutinasClient.ChargerRutinas();
             cmbRutine.DisplayMember = "Rutina";
-            cmbSport.ValueMember = "Id";
+            cmbSport.ValueMember = "id";
         }
         #endregion
 
