7f8695e0ee6bb9fc6749f71fb6347cad2ee0cf7f
Jaime Esquivel <jaimeesquivelgomez@gmail.com>
2015-04-20 16:55:12 -0600

    cargan los combox de los relacionales

--- TEAM_SOFTWARE_II/Project_View/Assigned Forms/frm_AssingRutina.cs
+++ TEAM_SOFTWARE_II/Project_View/Assigned Forms/frm_AssingRutina.cs
@@ -7,13 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Project_View.ServicioDeporte;
+using Project_View.ServicioParticipante;
+using Project_View.ServicioRutina;
 
 
 namespace Project_View
 {
     public partial class frm_AssingRutina : Form
     {
-       
+        ServicioDeporte.DeportesWebServiceClient oSportClient = new DeportesWebServiceClient();
+        ServicioRutina.RutinasClient oRutinasClient = new RutinasClient();
+        ServicioParticipante.ParticipantesWebServiceClient oParticipanteClient = new ParticipantesWebServiceClient();
         public frm_AssingRutina()
         {
             InitializeComponent();
@@ -34,21 +39,23 @@ namespace Project_View
         public void charge_Objects() 
         {
             //Cargar Combox
-           
+            cmbSports.DataSource = oSportClient.ChargeSportsList();
             cmbSports.DisplayMember = "Deporte";
-            cmbSports.ValueMember = "Id";
+            cmbSports.ValueMember = "id";
 
-            
+
+            cmbRutina.DataSource = oRutinasClient.ChargerRutinas(); 
             cmbRutina.DisplayMember = "Rutina";
             cmbRutina.ValueMember = "Id";
             //carga Dartagrid
            
-            dtgParticipantes.Columns[0].Visible = false;
-            dtgParticipantes.Columns[1].HeaderText = "Nombre";
-            dtgParticipantes.Columns[2].HeaderText = "Primer Apellido";
-            dtgParticipantes.Columns[3].HeaderText = "Segundo Apellido";
-            dtgParticipantes.Columns[4].HeaderText = "Edad";
-            dtgParticipantes.Columns[5].HeaderText = "Fecha Ingreso";
+
+            //dtgParticipantes.Columns[0].Visible = false;
+            //dtgParticipantes.Columns[1].HeaderText = "Nombre";
+            //dtgParticipantes.Columns[2].HeaderText = "Primer Apellido";
+            //dtgParticipantes.Columns[3].HeaderText = "Segundo Apellido";
+            //dtgParticipantes.Columns[4].HeaderText = "Edad";
+            //dtgParticipantes.Columns[5].HeaderText = "Fecha Ingreso";
         }
         #endregion
 
